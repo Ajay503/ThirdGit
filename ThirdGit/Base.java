9631a9ab9577afafb3bec07eecb1edf8431e51a5
Ajay <meenahalliajay007@gmail.com>
2018-10-28 10:00:59 +0530

    SECOND UPDATE

---
+++ ThirdGit/Base.java
@@ -0,0 +1,15 @@
+package com.project3.constants;
+
+public class Base {
+
+	public static void main(String[] ar)
+	{
+		
+		System.out.println("ajay kumar");
+		
+		
+	}
+	
+	
+	
+}
--- ThirdGit/src/com/project3/constants/Base.java
+++ ThirdGit/src/com/project3/constants/Base.java
@@ -2,4 +2,11 @@ package com.project3.constants;
 
 public class Base {
 
+
+
+public static void main(String[] ar)
+
+{
+	System.out.println("aJAY");
+}
 }
